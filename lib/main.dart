de8af7f0b50e0344edb42cfb850f01584613febb
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-22 22:26:43 +0300

    add main application entry point with provider setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/auth_provider.dart';
+import 'providers/request_provider.dart';
+import 'providers/quote_provider.dart';
+import 'app.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => RequestProvider()),
+        ChangeNotifierProvider(create: (_) => QuoteProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
