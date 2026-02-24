d9ca602e5bd079bd1f5b40d44235245589379448
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-25 00:33:07 +0300

    feat: Implement CustomerDirectoryProvider for managing customer data

---
+++ lib/providers/customer_directory_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../data/models/user_model.dart';
+import '../data/repositories/customer_repository.dart';
+
+class CustomerDirectoryProvider extends ChangeNotifier {
+  CustomerDirectoryProvider({required this.repository});
+
+  final CustomerRepository repository;
+
+  List<UserModel> _customers = [];
+  bool _isLoading = false;
+
+  List<UserModel> get customers => _customers;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadCustomers() async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      _customers = await repository.fetchCustomers();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  UserModel? getCustomerById(String id) {
+    try {
+      return _customers.firstWhere((c) => c.id == id);
+    } catch (_) {
+      return repository.getCustomerById(id);
+    }
+  }
+
+  void upsertCustomer(UserModel customer) {
+    final index = _customers.indexWhere((c) => c.id == customer.id);
+    if (index == -1) {
+      _customers.add(customer);
+    } else {
+      _customers[index] = customer;
+    }
+    repository.upsertCustomer(customer);
+    notifyListeners();
+  }
+}
