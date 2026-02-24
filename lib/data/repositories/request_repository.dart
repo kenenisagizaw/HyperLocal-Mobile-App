b58deef2cf659892b9bcd1b56ea64361ad4bad62
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-24 04:43:03 +0300

    feat: Implement RequestRepository class with methods to create and fetch service requests

--- lib/data/repositories/request_repository.dart
+++ lib/data/repositories/request_repository.dart
@@ -1,1 +1,16 @@
+import '../datasources/remote/request_api.dart';
+import '../models/service_request_model.dart';
 
+class RequestRepository {
+  final RequestApi api;
+
+  RequestRepository(this.api);
+
+  Future<void> createRequest(ServiceRequest request) {
+    return api.createRequest(request);
+  }
+
+  Future<List<ServiceRequest>> fetchRequests() {
+    return api.getRequests();
+  }
+}
