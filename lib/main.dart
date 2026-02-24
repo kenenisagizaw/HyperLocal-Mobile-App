fcbd73d5803d03604023616df56b4f1d0e3e7168
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-24 04:42:01 +0300

    feat: Add RequestApi and RequestRepository providers to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'data/datasources/remote/request_api.dart';
+import 'data/repositories/request_repository.dart';
 import 'providers/auth_provider.dart';
 import 'providers/request_provider.dart';
 import 'providers/quote_provider.dart';
@@ -9,8 +11,21 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
+        Provider(create: (_) => RequestApi()),
+        Provider(
+          create: (context) => RequestRepository(
+            context.read<RequestApi>(),
+          ),
+        ),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => RequestProvider()),
+        ChangeNotifierProxyProvider<RequestRepository, RequestProvider>(
+          create: (context) => RequestProvider(
+            repository: context.read<RequestRepository>(),
+          ),
+          update: (context, repository, previous) {
+            return previous ?? RequestProvider(repository: repository);
+          },
+        ),
         ChangeNotifierProvider(create: (_) => QuoteProvider()),
       ],
       child: const MyApp(),
