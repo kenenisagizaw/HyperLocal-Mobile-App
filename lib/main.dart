ec9df5da7eb21e700c266855cb13dc18e7046fd1
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-24 04:42:07 +0300

    style: Organize imports and format code for improved readability in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'app.dart';
 import 'data/datasources/remote/request_api.dart';
 import 'data/repositories/request_repository.dart';
 import 'providers/auth_provider.dart';
-import 'providers/request_provider.dart';
 import 'providers/quote_provider.dart';
-import 'app.dart';
+import 'providers/request_provider.dart';
 
 void main() {
   runApp(
@@ -13,15 +14,12 @@ void main() {
       providers: [
         Provider(create: (_) => RequestApi()),
         Provider(
-          create: (context) => RequestRepository(
-            context.read<RequestApi>(),
-          ),
+          create: (context) => RequestRepository(context.read<RequestApi>()),
         ),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProxyProvider<RequestRepository, RequestProvider>(
-          create: (context) => RequestProvider(
-            repository: context.read<RequestRepository>(),
-          ),
+          create: (context) =>
+              RequestProvider(repository: context.read<RequestRepository>()),
           update: (context, repository, previous) {
             return previous ?? RequestProvider(repository: repository);
           },
