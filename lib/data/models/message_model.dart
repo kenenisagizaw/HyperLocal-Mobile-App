58c700586f26686a0e0a3afc1a4a76c12ab0a134
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-25 00:29:08 +0300

    feat: Define Message model with properties and constructor

--- lib/data/models/message_model.dart
+++ lib/data/models/message_model.dart
@@ -1,1 +1,18 @@
+class Message {
+	final String id;
+	final String senderId;
+	final String receiverId;
+	final String content;
+	final DateTime createdAt;
+	bool isRead;
+
+	Message({
+		required this.id,
+		required this.senderId,
+		required this.receiverId,
+		required this.content,
+		required this.createdAt,
+		this.isRead = false,
+	});
+}
 
