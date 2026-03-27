cad0a93a4cd703a7a78b826fcedddb7926e190a3
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-03-27 21:08:15 +0300

    feat: Add UserApi class for managing user profiles and avatar uploads

---
+++ lib/data/datasources/remote/user_api.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../../core/constants/api_constants.dart';
+
+class UserApi {
+  final Dio _dio;
+
+  UserApi(this._dio);
+
+  Future<Map<String, dynamic>> getProfile({required String accessToken}) async {
+    final response = await _dio.get(
+      ApiConstants.userProfile,
+      options: Options(headers: _authHeaders(accessToken)),
+    );
+    return _asMap(response.data);
+  }
+
+  Future<Map<String, dynamic>> updateProfile({
+    required String accessToken,
+    required Map<String, dynamic> data,
+  }) async {
+    final response = await _dio.patch(
+      ApiConstants.userProfile,
+      data: data,
+      options: Options(headers: _authHeaders(accessToken)),
+    );
+    return _asMap(response.data);
+  }
+
+  Future<Map<String, dynamic>> uploadAvatar({
+    required String accessToken,
+    required XFile file,
+  }) async {
+    final formData = FormData.fromMap({
+      'avatar': await MultipartFile.fromFile(file.path, filename: file.name),
+    });
+
+    final response = await _dio.post(
+      ApiConstants.userProfileAvatar,
+      data: formData,
+      options: Options(headers: _authHeaders(accessToken)),
+    );
+
+    return _asMap(response.data);
+  }
+
+  Map<String, String> _authHeaders(String token) {
+    return {'Authorization': 'Bearer $token'};
+  }
+
+  Map<String, dynamic> _asMap(dynamic data) {
+    if (data is Map<String, dynamic>) {
+      return data;
+    }
+    if (data is Map) {
+      return data.cast<String, dynamic>();
+    }
+    return <String, dynamic>{};
+  }
+}
