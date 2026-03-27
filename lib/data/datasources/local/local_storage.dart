d6c78fd2ba2d6af432d37c178bfdf50477a33eb2
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-03-27 21:07:50 +0300

    feat: Implement LocalStorage class for managing access token storage

--- lib/data/datasources/local/local_storage.dart
+++ lib/data/datasources/local/local_storage.dart
@@ -1,1 +1,19 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+class LocalStorage {
+  static const String _accessTokenKey = 'access_token';
+
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+
+  Future<void> saveAccessToken(String token) async {
+    await _secureStorage.write(key: _accessTokenKey, value: token);
+  }
+
+  Future<String?> getAccessToken() async {
+    return _secureStorage.read(key: _accessTokenKey);
+  }
+
+  Future<void> clearAccessToken() async {
+    await _secureStorage.delete(key: _accessTokenKey);
+  }
+}
