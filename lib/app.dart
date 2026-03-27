9e7c2b21c085e3d27af09869da939e0b16925917
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-03-27 21:06:56 +0300

    feat: Remove AuthGate and related imports for simplified navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'core/theme/app_theme.dart';
-import 'data/models/user_model.dart';
-import 'features/auth/login_screen.dart';
-import 'features/auth/providers/auth_provider.dart';
-import 'features/customer/customer_dashboard.dart';
-import 'features/provider/provider_dashboard.dart';
 import 'routes.dart';
 
 class MyApp extends StatelessWidget {
@@ -18,26 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'Hyperlocal Marketplace',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
+      initialRoute: Routes.root,
       routes: Routes.routes,
     );
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final authProvider = context.watch<AuthProvider>();
-    final user = authProvider.currentUser;
-
-    if (user == null) {
-      return const LoginScreen();
-    }
-
-    return user.role == UserRole.customer
-        ? const CustomerDashboard()
-        : const ProviderDashboard();
   }
 }
