b69d82cafe45b6ac38d43c1d2059863bfa2a2a43
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-03-30 15:02:58 +0300

    refactor: Standardize formatting and indentation in QuoteRepository methods

--- lib/data/repositories/quote_repository.dart
+++ lib/data/repositories/quote_repository.dart
@@ -2,44 +2,43 @@ import '../datasources/remote/quote_api.dart';
 import '../models/quote_model.dart';
 
 class QuoteRepository {
-	QuoteRepository(this.api);
+  QuoteRepository(this.api);
 
-	final QuoteApi api;
+  final QuoteApi api;
 
-	Future<Quote> submitQuote({
-		required String serviceRequestId,
-		required double price,
-		required String message,
-		required int estimatedTime,
-	}) {
-		return api.submitQuote(
-			serviceRequestId: serviceRequestId,
-			price: price,
-			message: message,
-			estimatedTime: estimatedTime,
-		);
-	}
+  Future<Quote> submitQuote({
+    required String serviceRequestId,
+    required double price,
+    required String message,
+    required int estimatedTime,
+  }) {
+    return api.submitQuote(
+      serviceRequestId: serviceRequestId,
+      price: price,
+      message: message,
+      estimatedTime: estimatedTime,
+    );
+  }
 
-	Future<List<Quote>> fetchMyQuotes({int? take, int? skip}) {
-		return api.getMyQuotes(take: take, skip: skip);
-	}
+  Future<List<Quote>> fetchMyQuotes({int? take, int? skip}) {
+    return api.getMyQuotes(take: take, skip: skip);
+  }
 
-	Future<List<Quote>> fetchQuotesForRequest(String serviceRequestId) {
-		return api.getQuotesForRequest(serviceRequestId);
-	}
+  Future<List<Quote>> fetchQuotesForRequest(String serviceRequestId) {
+    return api.getQuotesForRequest(serviceRequestId);
+  }
 
-	Future<Quote?> acceptQuote({
-		required String requestId,
-		required String quoteId,
-	}) {
-		return api.acceptQuote(requestId: requestId, quoteId: quoteId);
-	}
+  Future<Quote?> acceptQuote({
+    required String requestId,
+    required String quoteId,
+  }) {
+    return api.acceptQuote(requestId: requestId, quoteId: quoteId);
+  }
 
-	Future<Quote?> withdrawQuote({
-		required String quoteId,
-		required String reason,
-	}) {
-		return api.withdrawQuote(quoteId: quoteId, reason: reason);
-	}
+  Future<Quote?> withdrawQuote({
+    required String quoteId,
+    required String reason,
+  }) {
+    return api.withdrawQuote(quoteId: quoteId, reason: reason);
+  }
 }
-
