be2fe98d94c8ce0348e94998d9495e3d4f496258
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-03-31 22:04:24 +0300

    refactor: Add BookingStatus enum to define booking states

--- lib/core/constants/enums.dart
+++ lib/core/constants/enums.dart
@@ -1,3 +1,5 @@
 enum RequestStatus { pending, quoted, accepted, completed, cancelled }
 
 enum QuoteStatus { pending, accepted, withdrawn, rejected }
+
+enum BookingStatus { booked, inProgress, completed, cancelled }
