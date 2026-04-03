e1dcdec99c9606f0087d7ae743b9aff13cbb8a5d
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-04-03 21:50:34 +0300

    feat: Load user requests alongside bookings in MyRequestsScreen

--- lib/features/customer/my_requests_screen.dart
+++ lib/features/customer/my_requests_screen.dart
@@ -24,6 +24,7 @@ class _MyRequestsScreenState extends State<MyRequestsScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (!mounted) return;
       context.read<RequestProvider>().loadMyRequests();
+      context.read<BookingProvider>().loadMyBookings();
     });
   }
 
