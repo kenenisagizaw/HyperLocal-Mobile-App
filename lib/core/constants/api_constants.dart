3b0c708d80ea907c3ce53fc8618bd87c76b99388
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-04-09 14:44:23 +0300

    fix: Update base URL in ApiConstants for correct API endpoint

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.137.133:5000';
+  static const String baseUrl = 'http://192.168.137.134:5000';
 
   static const String authBase = '/api/auth';
   static const String register = '$authBase/register';
