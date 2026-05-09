74b993ae438aab3a40b6e5cdc22367b1b7efd756
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-05-10 01:09:40 +0300

    feat: Implement ConnectsRepository for fetching connect wallet data

---
+++ lib/data/repositories/connects_repository.dart
@@ -0,0 +1,19 @@
+import '../datasources/remote/connects_api.dart';
+import '../models/connect_wallet_model.dart';
+import '../../core/utils/logger.dart';
+
+class ConnectsRepository {
+  ConnectsRepository(this._api);
+
+  final ConnectsApi _api;
+
+  Future<ConnectWallet> fetchConnects({int? skip, int? take}) async {
+    try {
+      Logger.info('Connects Repository - Fetching connects');
+      return await _api.fetchConnects(skip: skip, take: take);
+    } catch (e) {
+      Logger.error('Connects Repository - Failed to fetch connects: $e');
+      rethrow;
+    }
+  }
+}
